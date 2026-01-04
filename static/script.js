0c5376256121b514d2e56033fee268828c4bf658
RongalaSreePrasanna <rongalasreeprasanna@gmail.com>
2026-01-04 10:02:41 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,2 +1,10 @@
-// Basic JavaScript
-console.log('Script loaded');
+// Add any common JavaScript functionality here
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+    
+    // Any other common JS code
+});
